05860681ea6c7f734b6e689e85a870f7109d1f75
Michael Gruz <michael-gruz@seznam.cz>
2011-10-08 18:54:06 +0200

    Add auto create symlink to share library

---
+++ make-symlink.sh
@@ -0,0 +1,21 @@
+#/bin/sh
+
+find . -name lib*.so.* -print > file_lists2
+
+val0=$(pwd)
+for fn in `cat file_lists2`; do
+	val1=`echo ${fn} | cut -d/ -f2`
+	val2=`echo ${fn} | cut -d/ -f3`
+	val3=`echo ${fn} | cut -d/ -f4`
+	val4=`echo ${val3} | cut -d. -f1`
+	val5=`echo ${val3} | cut -d. -f2`
+	val6=${val4}.${val5}
+	val7=${val0}/${val1}/${val2}
+	echo "Create symlink from " ${val3} " to " ${val6}
+	cd ${val7}
+	rm -f ${val6}
+	ln -s ${val3} ${val6}
+done
+
+cd ${val0}
+rm -f file_lists2
